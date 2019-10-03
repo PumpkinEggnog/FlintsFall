dc91241ce5d46474ee1374416bb24457fd02ffb3
unknown <ahatnamedtrilby@gmail.com>
2019-10-03 17:33:11 -0500

    killing mutant

---
+++ Game Design Game/Assets/Scripts/EnemyHitboxHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHitboxHandler : MonoBehaviour
+{
+    public HealthSystem health = new HealthSystem(1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health.getHealth() <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        HitBox hitbox = other.gameObject.GetComponent<HitBox>();
+        this.health.changeHealth(hitbox.value);
+        Debug.Log("enemy health "+health.getHealth());
+    }
+}
