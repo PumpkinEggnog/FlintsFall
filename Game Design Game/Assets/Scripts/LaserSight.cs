a8dd0a0c3428bdc0f13e3c9c4a8839d3e358c71d
ConscriptGreen <bjimene1998@gmail.com>
2019-11-22 15:07:08 -0600

    Its highnoon
    Implements RaiderAI with functioning lasersight

---
+++ Game Design Game/Assets/Scripts/LaserSight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSight : MonoBehaviour
+{
+    //public Vector3 sphereRadius;
+    LineRenderer line;
+    RaiderAI raiderAI;
+    void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+        raiderAI = GetComponent<RaiderAI>();
+    }
+
+    void FixedUpdate()
+    {
+        line.enabled = true;
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        line.SetPosition(0, ray.origin);
+
+        if (Physics.Raycast(ray, out hit, 100))
+            line.SetPosition(1, hit.point);
+        else
+            line.SetPosition(1, ray.GetPoint(100));
+    }
+
+}
