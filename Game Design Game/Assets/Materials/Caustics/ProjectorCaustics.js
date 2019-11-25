512dbced7dfab8947476d9e77b19cbf9cf557896
awrohan <awrohan@users.noreply.github.com>
2019-11-25 12:40:55 -0600

    Vertex shader additions, colored shambler

---
+++ Game Design Game/Assets/Materials/Caustics/ProjectorCaustics.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+var pr:Projector;
+var FramesPerSecond:int = 30;
+var causticsTextures:Texture[] = null;
+//@script ExecuteInEditMode()
+function Start () {
+	pr = GetComponent.<Projector>();
+}
+
+function Update () {
+	var causticsIndex:int = (Time.time * FramesPerSecond) % causticsTextures.Length;
+	pr.material.SetTexture("_MainTex", causticsTextures[causticsIndex]);
+}
